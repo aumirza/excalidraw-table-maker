ee71312497517b889319007d8d478436eee4be82
Ahmadullah Mirza <mirzaahmad9028.AM@gmail.com>
2025-09-03 17:22:56 +0000

    add     // {     //     "src": "android_chrome_192X192.png",     //

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,43 @@ import { Toaster } from "@/components/ui/toaster";
 export const metadata: Metadata = {
   title: 'Excalidraw Table Maker',
   description: 'Create/Generate Excalidraw Table with AI assistance.',
+  icons: [
+    {
+        "url": "/android_chrome_192X192.png",
+        "sizes": "192x192",
+        "type": "image/png"
+    },
+    {
+        "url": "/android_chrome_512X512.png",
+        "sizes": "512x512",
+        "type": "image/png"
+    },
+    {
+        "url": "/apple_touch_icon.png",
+        "sizes": "180x180",
+        "type": "image/png"
+    },
+    {
+        "url": "/favicon_16X16.png",
+        "sizes": "16x16",
+        "type": "image/png"
+    },
+    {
+        "url": "/favicon_32X32.png",
+        "sizes": "32x32",
+        "type": "image/png"
+    },
+    {
+        "url": "/favicon_96X96.png",
+        "sizes": "96x96",
+        "type": "image/png"
+    },
+    {
+        "url": "/mstile_150X150.png",
+        "sizes": "150x150",
+        "type": "image/png"
+    },
+  ],
 };
 
 export default function RootLayout({
